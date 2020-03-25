2671821737edc8124a4c3a99a8078409b4188807
Artyom <3temal@gmail.com>
2020-03-25 15:46:16 +0300

    Add docker support for Identity.API

--- Services/Identity/Identity.API/Program.cs
+++ Services/Identity/Identity.API/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -17,10 +18,20 @@ namespace Identity.API
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args)                
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .ConfigureKestrel(serverOptions =>
+                        {
+                            serverOptions.AllowSynchronousIO = true;
+                            serverOptions.ConfigureHttpsDefaults(listenOptions =>
+                            {
+                                // certificate is an X509Certificate2
+                                listenOptions.ServerCertificate = Certificate.Certificate.Get();                                
+                            });
+                        })
+                        .UseStartup<Startup>();
                 });
     }
 }
