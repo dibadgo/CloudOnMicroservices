1b3ca6f7cbf8b40a1abc51ca6d497b2551820bf8
Artyom <3temal@gmail.com>
2020-04-13 14:36:41 +0300

    Add deploy script Add docker-compose

---
+++ deploy-linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PROJECT_HOME=/opt/incloud
+
+echo "Making project folder /opt/incloud"
+mkdir $PROJECT_HOME
+
+# Preparing volumes
+
+echo "Making volumes folder for MS SQL Server"
+mkdir -p $PROJECT_HOME/db/mssqlserver{data,log,secrets}
+chmod -v -R 666 $PROJECT_HOME/db/mssqlserver/*
+# We should provide the permissions on read/write for all users 
+# to avoid the error like 'System cannot find the specified file' from MS SQL Server
+
+echo "Making volumes folder for Redis"
+mkdir -p $PROJECT_HOME/db/redis/data
+
+echo "Ok, let's build and run services..."
+docker-compose up -d
